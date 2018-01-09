df42b6d2d97e2ff6de4feadb584531a4a37267c1
rrodgers <rrodgers0827@outlook.com>
2018-01-09 13:05:51 -0600

    working through course

---
+++ client/components/SongList.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import { Link, hashHistory } from 'react-router';
+
+import fetchSongs from '../queries/fetchSongs';
+import deleteSong from '../mutations/deleteSong';
+
+
+class SongList extends Component {
+  onDeleteClick(id) {
+    console.log(this.props);
+    this.props.mutate({
+      variables: { id }
+    }).then(() => this.props.data.refetch());
+  }
+
+  renderSongs() {
+    const { songs } = this.props.data;
+    return this.props.data.songs.map(song => {
+      const { id, title } = song;
+      return (
+        <li key={id} className={'collection-item'}>
+          {title}
+          <i
+            className='material-icons'
+            onClick={() => this.onDeleteClick(id)}
+          >
+            delete
+          </i>
+        </li>
+      )
+    })
+  }
+
+  render() {
+    if (this.props.data.loading) {
+      return <div>loading...</div>;
+    }
+    return (
+      <div>
+        <ul className={'collection'}>
+          {this.renderSongs()}
+        </ul>
+        <Link
+          to="/songs/new"
+          className='btn-floating btn-large red right'
+        >
+          <i className='material-icons'>add</i>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default graphql(deleteSong)(
+  graphql(fetchSongs)(SongList)
+);
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://russ:ChangeMe!0@ds061787.mlab.com:61787/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
